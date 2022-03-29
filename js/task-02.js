7e8440d09bcd4bdf3a41e5a6dd654a81b96b8d94
DmitryDavidyuk <dima.demon.95@gmail.com>
2022-03-29 13:47:17 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+//получаем ссылку на список
+const list = document.querySelector('#ingredients')
+
+
+const elements = ingredients.map(ingredient => {
+  const listEl = document.createElement("li");
+  listEl.textContent = ingredient;
+  listEl.classList.add('item');
+  
+  return listEl;
+})
+
+list.append(...elements)ж
