6fc9e529d2192a7e43bd6e00763faa25f39809dc
DmitryDavidyuk <dima.demon.95@gmail.com>
2022-04-11 21:45:35 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('body');
+const changeBtnEl = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color')
+
+changeBtnEl.addEventListener('click', () => {
+  const randomBgc = body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = randomBgc
+})
