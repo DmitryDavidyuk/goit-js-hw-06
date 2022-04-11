5ac91f325b1e66431ba150aae6c94d3ecc80c226
DmitryDavidyuk <dima.demon.95@gmail.com>
2022-04-11 18:20:18 +0300

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector('#name-input');
+const textOutput = document.querySelector('#name-output');
+
+textInput.addEventListener('input', (event) => {
+    textOutput.textContent = event.currentTarget.value;
+    
+    if (textInput.value === '') {
+        textOutput.textContent = 'Anonymous';
+    }
+})
+
